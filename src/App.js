294602e86241ba26ed354431d6d564fdfd252e31
romulosanttos <romulosanttos1@gmail.com>
2020-01-18 23:46:36 -0300

    terminando layout da pagina

--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input name="github_username" id="username_github" required />
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias </label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div class="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/11372354?s=460&v=4"
+                alt="Romulo Santos"
+              />
+              <div className="user-info">
+                <strong>Romulo Santos</strong>
+                <span>ReactJS, React Native, Node.js, Graphql</span>
+              </div>
+            </header>
+            <p>Full Stack Developer</p>
+            <a href="https://github.com/Romulosanttos">
+              Acessar perfil no GitHub
+            </a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/11372354?s=460&v=4"
+                alt="Romulo Santos"
+              />
+              <div className="user-info">
+                <strong>Romulo Santos</strong>
+                <span>ReactJS, React Native, Node.js, Graphql</span>
+              </div>
+            </header>
+            <p>Full Stack Developer</p>
+            <a href="https://github.com/Romulosanttos">
+              Acessar perfil no GitHub
+            </a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/11372354?s=460&v=4"
+                alt="Romulo Santos"
+              />
+              <div className="user-info">
+                <strong>Romulo Santos</strong>
+                <span>ReactJS, React Native, Node.js, Graphql</span>
+              </div>
+            </header>
+            <p>Full Stack Developer</p>
+            <a href="https://github.com/Romulosanttos">
+              Acessar perfil no GitHub
+            </a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/11372354?s=460&v=4"
+                alt="Romulo Santos"
+              />
+              <div className="user-info">
+                <strong>Romulo Santos</strong>
+                <span>ReactJS, React Native, Node.js, Graphql</span>
+              </div>
+            </header>
+            <p>Full Stack Developer</p>
+            <a href="https://github.com/Romulosanttos">
+              Acessar perfil no GitHub
+            </a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
